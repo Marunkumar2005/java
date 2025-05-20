e5e3d556df2a9381e59274d4ae16f1d03d3d0e2c
Arun Kumar <160826741+Marunkumar2005@users.noreply.github.com>
2025-05-20 22:19:07 +0530

    Create Steps.java

---
+++ recursion/recursionkunalday2/Steps.java
@@ -0,0 +1,23 @@
+public class Steps {
+    public int numberOfSteps(int num) {
+        return helper(num, 0);
+
+    }
+
+    public int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+
+        }
+        if (num % 2 == 0) {
+            return helper(num / 2, steps + 1);
+
+        }
+        return helper(num - 1, steps + 1);
+
+    }
+
+    public static void main(String[] args) {
+        numberOfSteps(12331);
+    }
+}
