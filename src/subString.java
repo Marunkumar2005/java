315eb48ffe38bf600e9e644261dde45d104660e1
arun kumar <arunkumarmak651@gmail.com>
2025-06-15 08:56:42 +0530

    files are commited

---
+++ src/subString.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class subString {
+
+    static List<List<Integer>> SubSeq(int [] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>();
+
+            }
+
+        }
+return new ArrayList<>();
+    }
+    public static void main(String[] args) {
+
+    }
+}
