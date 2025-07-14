3468c0434a3ccc9d47a975cf4026a15cab5c7d77
Arun Kumar <160826741+Marunkumar2005@users.noreply.github.com>
2025-07-14 22:47:10 +0530

    Create allPaths.java

---
+++ backtrackingkunal/allPaths.java
@@ -0,0 +1,43 @@
+package backtracking;
+
+public class allPaths {
+
+    static void allPath(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+
+        }
+        if (!maze[r][c]) {
+            return;
+
+        }
+        //iM CONSIDERING THIS BLOCK in my path
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            allPath(p + 'D', maze, r + 1, c);
+        }
+        if (c < maze[0].length - 1) {
+            allPath(p + 'R', maze, r, c + 1);
+        }
+        if (r > 0) {
+            allPath(p + 'U', maze, r - 1, c);
+        }
+        if (c > 0) {
+            allPath(p + 'L', maze, r, c - 1);
+        }
+
+        //this line is where the finction will be over
+        //so befire the function gets removed ,also remove the changes that were made by that function
+        maze[r][c] = true;
+
+
+    }
+
+    public static void main(String[] args) {
+        boolean[][] maze = {{true, true, true}, {true, true, true}, {true, true, true}};
+        allPath("", maze, 0, 0);
+    }
+}
+
